5289a5f12e81ffc5ef27574642af28744fe7db63
tatsuya551 <seki0515ya@gmail.com>
2020-02-17 15:05:19 +0900

    cross_search

--- app/models/board.rb
+++ app/models/board.rb
@@ -5,4 +5,14 @@ class Board < ApplicationRecord
   has_many :categories, through: :board_category_relationships
 
   validates :title, presence: true
+
+  def self.search(search_keyword)
+    split_keyword = search_keyword.split(/[[:blank:]]+/)
+    match_keywords = []
+    split_keyword.each do |keyword|
+      next if keyword == "" 
+      match_keywords += self.eager_load(:responses).where(['title LIKE ? OR responses.comment LIKE ?', "%#{keyword}%", "%#{keyword}%" ])
+    end
+    match_keywords.uniq
+  end
 end
